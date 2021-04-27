066b01739c6e2add233aab17306fb7b7db3955d5
Lapokute <31049049+Lapokute@users.noreply.github.com>
2021-04-27 07:11:05 -0400

    fixed

--- p3/js/main.js
+++ p3/js/main.js
@@ -1,26 +1,26 @@
-// $(document).ready(function() {
-//   $("h1").hide();
-//   $(window).click(function() {
-//     $("h1.one").show();
-//   });
-// next();
-// });
-// function next() {
-//   $('.bob').click(function() {
-//     $("h1.two").show();
-//     $().removeclass("h1.one")
-// });
-//
-// }
-
 $(document).ready(function() {
-    var quotes = new Array("No one's around to help", "bar", "baz", "chuck"),
-    randno = quotes[Math.floor( Math.random() * quotes.length )];
-    $("h1#one").text( randno );
-    $('.bob').click(function() {
-      location.reload(true);
-    });
+  // $("h1").hide();
+  $(window).click(function() {
+    $("h1.one").show();
+  });
+next();
 });
+function next() {
+  $('.bob').click(function() {
+    $("h1.two").show();
+    $().removeclass("h1.one")
+});
+
+}
+//
+// $(document).ready(function() {
+//     var quotes = new Array("No one's around to help", "bar", "baz", "chuck"),
+//     randno = quotes[Math.floor( Math.random() * quotes.length )];
+//     $("h1#one").text( randno );
+//     $('.bob').click(function() {
+//       location.reload(true);
+//     });
+// });
 
 // function randomImage1() {
 // 	var imgArray = ["img/Puffins-1.jpg","img/Puffins-2.jpg","img/Puffins-3.jpg","img/Puffins-4.jpg","img/Puffins-5.jpg","img/Puffins-6.jpg","img/Puffins-7.jpg","img/Puffins-8.jpg"];
