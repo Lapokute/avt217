c27a480b77d45035445ce0b03a44c91e73611766
Lapokute <31049049+Lapokute@users.noreply.github.com>
2021-04-27 00:22:30 -0400

    omfucking god i hate me

--- e14/js/main.js
+++ e14/js/main.js
@@ -3,62 +3,79 @@ $(document).ready(function() {
   $("h1").hide();
   $("p").hide();
 
-  $('.four').click(function() {
+  $('.circ4').click(function() {
     $('.four').toggleClass('ono4');
-      $("p").show();
-      $("h1").show();
+      $("p#vps").show();
+      $("h1#jp").show();
     $('.circ4').toggleClass('hidden');
   });
-  $('.five').click(function() {
+  $('.circ5').click(function() {
     $('.five').toggleClass('ono5');
-      $("p").show();
-      $("h1").show();
+      $("p#macross").show();
+      $("h1#youarenow").show();
     $('.circ5').toggleClass('hidden');
   });
-  $('.three').click(function() {
+  $('.circ3').click(function() {
     $('.three').toggleClass('ono3');
-      $("p").show();
-      $("h1").show();
+      $("p#psy").show();
+      $("h1#dont").show();
     $('.circ3').toggleClass('hidden');
   });
-  $('.two').click(function() {
+  $('.circ2').click(function() {
     $('.two').toggleClass('ono2');
-      $("p").show();
-      $("h1").show();
+      $("p#yung").show();
+      $("h1#wlcome").show();
     $('.circ2').toggleClass('hidden');
   });
-  $('.one').click(function() {
+  $('.circ1').click(function() {
     $('.one').toggleClass('ono1');
-      $("p").show();
-      $("h1").show();
+      $("p#sasa").show();
+      $("h1#tiger").show();
     $('.circ1').toggleClass('hidden');
   });
+  hover();
+  swatch();
 });
 
-    // run function on resize of the window
-
-
-//     function onmouseover() {
-//       $('.four').toggleClass('.circ4')
-//     }
-//     // run function on scroll of the window
-//     $(window).scroll(function() {
-//       scroll();
-//     });
-//
-// });
-//
-// $('.boxbox').mouseleave(function() {
-//   $("p").show();
-//   $("h1").show();
-// });
-//
-//
-function onmouseover() {
-  $("p").show();
-  $("h1").show();
+function hover() {
+  $('.one').on({"mouseleave": function(){
+    $('.ono1').toggleClass('oono1');
+    },
+  });
+  $('.two').on({"mouseleave": function(){
+    $('.ono2').toggleClass('oono2');
+    },
+  });
+  $('.three').on({"mouseleave": function(){
+    $('.ono3').toggleClass('oono3');
+    },
+  });
+  $('.four').on({"mouseleave": function(){
+    $('.ono4').toggleClass('oono4');
+    },
+  });
+  $('.five').on({"mouseleave": function(){
+    $('.ono5').toggleClass('oono5');
+    },
+  });
 }
-//
-// function scroll() {
-//   $('.square').removeClass('purple');
-// }
+function swatch() {
+  $("h1").dblclick(function(){
+    $('songs').toggleClass('smolsongs');
+    $('.one').toggleClass('nono1');
+    $("p#vps").hide();
+    $("h1#jp").hide();
+    $('.two').toggleClass('nono2');
+    $("p#macross").hide();
+    $("h1#youarenow").hide();
+    $('.three').toggleClass('nono3');
+    $("p#sasa").hide();
+    $("h1#tiger").hide();
+    $('.four').toggleClass('nono4');
+    $("p#psy").hide();
+    $("h1#dont").hide();
+    $('.five').toggleClass('nono5');
+    $("p#yung").hide();
+    $("h1#wlcome").hide();
+  });
+}
